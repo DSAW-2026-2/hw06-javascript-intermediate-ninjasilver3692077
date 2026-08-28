9d97568d812c3901a8826e5d1a59d1e975bc7da6
ninja <ninjasilver369@gmail.com>
2026-08-28 07:58:25 -0500

    added eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    ignores: ['template/**', '.grading/**', 'node_modules/**']
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        document: 'readonly',
+        retos: 'readonly'
+      }
+    },
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': 'error',
+      eqeqeq: 'error',
+      'no-eval': 'error',
+      'no-implied-eval': 'error'
+    }
+  }
+];
